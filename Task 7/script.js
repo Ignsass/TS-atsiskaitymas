8ab7aa7f03a8d3ddbbd1008722b639124e80966a
ignsass <ignsass@yahoo.com>
2024-07-19 13:10:11 +0300

    pakeistas tsconfig failas, kad nebutu var

--- Task 7/script.js
+++ Task 7/script.js
@@ -1,23 +1,12 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 const ENDPOINT = 'NBA.json';
-function fetchNBAData() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const response = yield fetch(ENDPOINT);
-        const data = yield response.json();
-        if (!data.teams || !Array.isArray(data.teams)) {
-            throw new Error('Invalid data format: expected an object with a "teams" array');
-        }
-        return data.teams;
-    });
+async function fetchNBAData() {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+    if (!data.teams || !Array.isArray(data.teams)) {
+        throw new Error('Invalid data format: expected an object with a "teams" array');
+    }
+    return data.teams;
 }
 function createTeamCard(team) {
     const teamCard = document.createElement('div');
@@ -46,21 +35,19 @@ function createPlayerCard(player) {
     playerCard.appendChild(playerInfoLink);
     return playerCard;
 }
-function renderNBAData() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const outputDiv = document.getElementById('output');
-        if (!outputDiv)
-            return;
-        try {
-            const teams = yield fetchNBAData();
-            teams.forEach(team => {
-                const teamCard = createTeamCard(team);
-                outputDiv.appendChild(teamCard);
-            });
-        }
-        catch (error) {
-            console.error('Error rendering NBA data:', error);
-        }
-    });
+async function renderNBAData() {
+    const outputDiv = document.getElementById('output');
+    if (!outputDiv)
+        return;
+    try {
+        const teams = await fetchNBAData();
+        teams.forEach(team => {
+            const teamCard = createTeamCard(team);
+            outputDiv.appendChild(teamCard);
+        });
+    }
+    catch (error) {
+        console.error('Error rendering NBA data:', error);
+    }
 }
 renderNBAData();
