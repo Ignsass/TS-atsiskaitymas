edc5178a32c90db5b7acba76f17bfc3e3f88d74d
ignsass <ignsass@yahoo.com>
2024-07-19 12:39:32 +0300

    js be komentaru

--- Task 3/script.js
+++ Task 3/script.js
@@ -1,10 +1,4 @@
 "use strict";
-/* ------------------------------ TASK 3 -----------------------------------
-Perpanaudokite aprašytą type'ą, kad sukurti naujus tipus, kuriuos priskyrus kintamiesiems, visas kodas veiktų teisingai.
-Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą. Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.
-
-Pastaba: Aprašyto tipo NEKEISTI
--------------------------------------------------------------------------- */
 const dviratis = {
     metai: 1999,
     spalva: ''
@@ -21,5 +15,4 @@ const senaMasina = {
     metai: 2025,
     spalva: '',
     kilometrazas: 999999,
-    // surudyjesDugnas: true  
 };
